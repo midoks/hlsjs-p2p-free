65d72ac6830705c2a1fb6eccebe3a7f7953687b7
midoks <midoks@163.com>
2021-11-06 03:55:45 +0800

    up

--- src/index.js
+++ src/index.js
@@ -1,4 +1,56 @@
-import wwws from './ws.js';
-import {sum,square} from './utils.js';
+// import wwws from './ws.js';
+// import {sum,square} from './utils.js';
+// console.log(sum(1,2));
+console.log("start...");
 
-console.log(sum(1,2));
+
+
+import Hlsjs from 'hls.js';
+
+let recommendedHlsjsConfig = {
+    maxBufferSize: 0,
+    maxBufferLength: 30,
+    liveSyncDuration: 30,
+    fragLoadingTimeOut: 4000,              // used by fragment-loader
+};
+
+class CDNByeHlsjs extends Hlsjs {
+
+    static get P2PEvents() {
+    	console.log(P2PEngine.Events);
+        return P2PEngine.Events;
+    }
+
+    static get uaParserResult() {
+        return P2PEngine.uaParserResult;
+    }
+
+    constructor(config = {}) {
+
+    	console.log(config);
+
+        let p2pConfig = config.p2pConfig || {};
+        delete config.p2pConfig;
+
+        let mergedHlsjsConfig = Object.assign({}, recommendedHlsjsConfig, config);
+
+        super(mergedHlsjsConfig);
+
+        // if (P2PEngine.WEBRTC_SUPPORT) {
+        //     this.engine = new P2PEngine(this, p2pConfig);
+        // }
+
+    }
+
+    enableP2P() {
+        this.engine.enableP2P();
+    }
+
+    disableP2P() {
+        this.engine.disableP2P();
+    }
+
+
+}
+
+export default CDNByeHlsjs;
--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,84 @@
 const pkgJson = require('./package.json');
 const path = require('path');
+const webpack = require('webpack');
 
+
+const uglifyJsOptions = {
+    screwIE8: true,
+    stats: true,
+    compress: {
+        warnings: false,
+        drop_debugger: true,
+        drop_console: true
+    },
+    mangle: {
+        toplevel: true,
+        eval: true
+    },
+    output: {
+        comments: false,  // remove all comments
+        preamble: "/* A P2P-CDN supporting hls player built on WebRTC Data Channels API. @author XieTing <86755838@qq.com> <https://github.com/snowinszu> */"
+    }
+};
+
+function getConstantsForConfig(type) {                                             //嵌入全局变量
+    return {
+        __VERSION__: JSON.stringify(pkgJson.version),
+    };
+}
+
+function getAliasesForLightDist() {
+    let aliases = {};
+
+    aliases = Object.assign({}, aliases, {
+        './bittorrent': './empty.js'
+    });
+
+
+    return aliases;
+}
+
+function getPluginsForConfig(minify = false, type) {
+    // common plugins.
+    const plugins = [
+        // new webpack.optimize.OccurrenceOrderPlugin(),
+        // new webpack.optimize.ModuleConcatenationPlugin(),
+        new webpack.DefinePlugin(getConstantsForConfig(type))
+    ];
+
+    if (minify) {
+        // minification plugins.
+        return plugins.concat([
+            // new webpack.optimize.UglifyJsPlugin(uglifyJsOptions),
+            new webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: true
+            })
+        ]);
+    }
+
+    return plugins;
+}
+
+const commonConfig = {
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: [
+                    path.resolve(__dirname, 'node_modules')
+                ]
+            }
+        ]
+    }
+};
 
 const multiConfig = [
   {
     name: 'debug',
     mode: 'development',
+    entry:"./src/index.js",
     output: {
       filename: 'p2p.js',
       chunkFilename: '[name].js',
@@ -18,8 +91,22 @@ const multiConfig = [
       globalObject: 'this',
     },
     devtool: 'source-map',
-  }
-];
+  },
+  {
+  	mode: 'production',
+    name: 'release',
+    entry: './src/index.js',
+    output: {
+    	chunkFilename: '[name].js',
+        filename: 'p2p.min.js',
+        path: path.resolve(__dirname, 'dist'),
+        // publicPath: '/src/',
+        library: ['Hls'],
+        libraryTarget: 'umd'
+    },
+    plugins: getPluginsForConfig(true)
+	},
+].map(fragment => Object.assign({}, commonConfig, fragment));;
 module.exports = (envArgs) => {
   const requestedConfigs = Object.keys(envArgs).filter(
     (key) => !/^WEBPACK_/.test(key)
